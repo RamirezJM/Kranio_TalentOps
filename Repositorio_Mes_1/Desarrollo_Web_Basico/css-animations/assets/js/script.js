15a6e1c9d9c45dcd83da82a277d22eadec6dc7d6
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-11-19 23:12:54 -0300

    feat: agregar carpeta y archivos del proyecto

---
+++ Repositorio_Mes_1/Desarrollo_Web_Basico/css-animations/assets/js/script.js
@@ -0,0 +1,33 @@
+const sidebar = document.getElementById("sidebar");
+const main = document.querySelector(".main");
+const toggleSidebar = document.getElementById("toggleSidebar");
+const closeSidebar = document.querySelector(".close-sidebar")
+const toggleTheme = document.getElementById("toggleTheme");
+const loader = document.getElementById("loader");
+const content = document.getElementById("content");
+
+// Toggle sidebar
+toggleSidebar.addEventListener("click", () => {
+  sidebar.classList.toggle("expanded");
+  /* sidebar.classList.toggle("expanded"); */
+});
+
+closeSidebar.addEventListener("click", () => {
+  sidebar.classList.remove("expanded");
+});
+
+// Toggle theme
+toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if(document.body.classList.contains('dark')){
+    toggleTheme.textContent = '🌞'
+  }else{
+    toggleTheme.textContent = '🌚'
+  }
+});
+
+// Fake loading
+setTimeout(() => {
+  loader.classList.add("hidden");
+  content.classList.remove("hidden");
+}, 1500);
